311a53fe8b8745c2a326a74c29c01889cefe5cf7
Ishan Jain <51803183+bitsofishan@users.noreply.github.com>
2020-09-03 21:52:48 +0530

    Update sum_of_squares.py

--- sum_of_squares.py
+++ sum_of_squares.py
@@ -1,13 +1,8 @@
 """Question - Write a short Python function that takes a positive integer n and returns
 the sum of the squares of all the positive integers smaller than n."""
 
-def sum_of_squares(data):
-    total=0
-    for i in range(1,data):
-        total=total+(i*i)
-        
-    return total
+def sum_of_squares(data):   
+    return sum((i*i) for i in range(1,data))
 
-
-squares_sum=sum_of_squares(100)
+squares_sum=sum_of_squares(2)
 print(squares_sum)

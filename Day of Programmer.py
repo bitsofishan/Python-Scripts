8b5a5c1a1d77cc5ef5d3d44154a97fb274282742
Ishan Jain <51803183+bitsofishan@users.noreply.github.com>
2020-10-01 13:32:04 +0530

    Update Day of Programmer.py

--- Day of Programmer.py
+++ Day of Programmer.py
@@ -8,8 +8,8 @@ import sys
 
 # Complete the dayOfProgrammer function below.
 def dayOfProgrammer(year):
- if year==1917:
-    return "26.09.1917"
+ if year==1918:
+    return "26.09.1918"
  elif (year<1917 and year%4==0) or (year>1918 and (year%400==0 or (year%4==0 and     year%100!=0))):
     return "12.09.%s"%year
  else:
